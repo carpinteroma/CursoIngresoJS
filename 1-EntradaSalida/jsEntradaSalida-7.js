517b025b7151746b43a36885793650a7b3f6aee8
carpinteroma <52534985+carpinteroma@users.noreply.github.com>
2019-07-11 12:25:27 -0300

    java

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -1,24 +1,60 @@
 /*Debemos lograr tomar Los numeros por ID ,
-transformarlos a enteros (parseInt),realizar la operación correcta y 
+transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
-ej.: "la Resta es 750"*/ 
+ej.: "la Resta es 750"*/
 function sumar()
-{	
-	
+{
+
+  var numero1;
+  numero1 = document.getElementById("numeroUno").value ;
+  numero1 = parseInt(numero1);
+
+  var numero2;
+  numero2 = document.getElementById("numeroDos").value ;
+  numero2 = parseInt(numero2);
+  alert(numero1 + numero2);
 }
+
+
 
 function restar()
 {
-	
+  var numero1;
+  numero1 = document.getElementById("numeroUno").value;
+  numero1 = parseInt(numero1);
+
+  var numero2;
+  numero2 = document.getElementById("numeroDos").value;
+  numero2 = parseInt(numero2);
+
+  alert (numero1 - numero2);
 }
 
 function multiplicar()
-{ 
-	
+{
+ var numero1;
+ numero1 = document.getElementById("numeroUno").value;
+ numero1 = parseInt(numero1);
+
+ var numero2;
+ numero2 = document.getElementById("numeroDos").value;
+ numero2 = parseInt(numero2);
+
+ alert (numero1 * numero2);
 }
+
 
 function dividir()
 {
-	
+   var numero1;
+
+   numero1 = document.getElementById("numeroUno").value;
+   numero1 = parseInt(numero1);
+
+   var numero2;
+   numero2 = document.getElementById("numeroDos").value;
+   numero2 = parseInt(numero2);
+
+   alert(numero1 / numero2);
 }
 
--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -1,8 +1,18 @@
 /*Debemos lograr tomar Los numeros por ID ,
-transformarlos a enteros (parseInt),realizar la operación correcta y 
+transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
 function SacarResto()
 {
-	
+
+  var numero1;
+  numero1 = document.getElementById("ingreseDividendo").value;
+  numero1 = parseInt(numero1);
+
+  var numero2;
+  numero2 = document.getElementById("ingreseDivisor").value;
+  numero2 = document.getElementById(numero2);
+
+  alert (numero1 % numero2 );
 }
+
